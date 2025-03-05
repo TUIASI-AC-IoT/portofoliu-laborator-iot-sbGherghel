f51e76fa92f1499407f3a429b3309342e2f97966
sbGherghel <sb.gherghel@gmail.com>
2025-03-05 19:38:07 +0200

    Add files via upload

---
+++ L3/include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "8"
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.8 - 2025-03-05 19:35:31.310213"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.8"
+#endif
